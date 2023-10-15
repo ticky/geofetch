fddcea9eccf5c074548d49eb311c0608fd49e0e6
Jessica Stokes <hello@jessicastokes.net>
2023-10-15 15:21:56 -0700

    Initial version of geofetch

---
+++ README.md
@@ -0,0 +1,45 @@
+# geofetch
+
+_with apologies to the neofetch maintainers and SNK Corporation_
+
+A system information utility for an extremely small subset of people.
+
+## WTF?
+
+It's a "system information" utility in that it _does_ show some system information, but in a format inspired by the classic NeoGeo boot screen. Your system's RAM size in _mega, giga or terabits_, and your OS. That's it.
+
+```
+  ____   _____    ___    _____   _____   _____    ____   _   _
+ / ___| | ____|  / _ \  |  ___| | ____| |_   _|  / ___| | | | |
+| |  _  |  _|   | | | | | |_    |  _|     | |   | |     | |_| |
+| |_| | | |___  | |_| | |  _|   | |___    | |   | |___  |  _  |
+ \____| |_____|  \___/  |_|     |_____|   |_|    \____| |_| |_|
+
+
+                         MAX 128 GIGA
+
+                        PRO-GEAR SPEC
+
+                            MAC OS
+
+```
+
+neofetch -> neogeo -> geofetch.
+
+## Installation
+
+geofetch is available from cargo:
+
+```bash
+cargo install geofetch
+```
+
+## Options and Configuration
+
+geofetch has no options or configuration.
+
+## Usage
+
+```bash
+geofetch
+```
---
+++ src/main.rs
@@ -0,0 +1,73 @@
+fn get_size() -> String {
+    use si_scale::prelude::{Base, Constraint, Value};
+    use systemstat::{Platform, System};
+
+    let sys = System::new();
+
+    let memory_bits = sys
+        .memory()
+        .expect("Failed to query memory information!")
+        .total
+        .as_u64()
+        * 8;
+
+    let mem_size = Value::new_with(memory_bits as f64, Base::B1024, Constraint::UnitAndAbove);
+    let prefix_string = format!("{:?}", mem_size.prefix).to_uppercase();
+
+    format!("{} {}", mem_size.mantissa, prefix_string)
+}
+
+fn get_os() -> String {
+    os_info::get().os_type().to_string().to_uppercase()
+}
+
+fn main() {
+    use figlet_rs::FIGfont;
+    use terminal_size::{terminal_size, Width};
+    use textflow::align;
+    use textflow::{Alignment, Options as TextflowOptions};
+
+    let textflow_options = match terminal_size() {
+        Some((Width(width), _)) => TextflowOptions::new(width.into()),
+        _ => TextflowOptions::new(80),
+    };
+
+    let standard_font = FIGfont::standard().unwrap();
+
+    let banner = "GEOFETCH";
+
+    let figure = match standard_font.convert(banner) {
+        Some(banner) => format!("{}", banner),
+        _ => banner.to_string(),
+    };
+
+    println!(
+        "{}",
+        align(&figure, Alignment::CENTER, textflow_options.clone())
+    );
+
+    let spec = format!("MAX {}", get_size());
+
+    println!(
+        "{}",
+        align(&spec, Alignment::CENTER, textflow_options.clone())
+    );
+
+    println!();
+
+    println!(
+        "{}",
+        align("PRO-GEAR SPEC", Alignment::CENTER, textflow_options.clone())
+    );
+
+    let os_type = get_os();
+
+    println!();
+
+    println!(
+        "{}",
+        align(&os_type, Alignment::CENTER, textflow_options.clone())
+    );
+
+    println!();
+}
